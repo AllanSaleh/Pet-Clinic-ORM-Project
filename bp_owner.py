cd129f6b1a5e2a7d4665bc54f764ea6dfe012c19
allan <allansaleh95@gmail.com>
2025-11-10 21:10:21 -0500

    delete owner function

--- bp_owner.py
+++ bp_owner.py
@@ -16,9 +16,42 @@ def view_owner(current_user):
 #allows user to update any of the fields
 #commits changes 
 #shows changes and returns update current_user
+def update_owner(current_user):
+  current_user.display()
+  
+  print("Fill in desired changes, to keep current values leave blank")
+  name = input("Name: ")
+  email = input("Email: ")
+  password = input("Password: ")
+  phone = input("Phone: ")
+
+  if name:
+    current_user.name = name
+  if email:
+    current_user.email = email
+  if password:
+    current_user.password = password
+  if phone:
+    current_user.phone = phone
+
+  session.commit() #Commit changes to the db
+  print("------------- Updated Info --------------")
+  current_user.display()
+  return current_user
 
 #Update profile function
 #Ask user to confirm they want to delete
 #if so delete the current user from the session
 #commits changes 
 #call main() to start the program over
+
+def delete_owner(current_user):
+  choice = input("To confirm you want to delete your account, type 'delete'")
+  if choice == 'delete':
+    session.delete(current_user)
+    session.commit()
+    print("Deleted account successfully")
+    return None
+  else:
+    print("You opted out, back to menu")
+    return current_user
